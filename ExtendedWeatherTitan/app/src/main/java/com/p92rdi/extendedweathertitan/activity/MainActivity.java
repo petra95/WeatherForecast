a8d4c6d564bc76fa800cb39f8a8ded19ccae4ea9
petra95 <antalicspetra@gmail.com>
2016-08-03 12:03:39 +0200

    start

---
+++ ExtendedWeatherTitan/app/src/main/java/com/p92rdi/extendedweathertitan/activity/MainActivity.java
@@ -0,0 +1,145 @@
+package com.p92rdi.extendedweathertitan.activity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.p92rdi.extendedweathertitan.R;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.search) {
+            SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+            searchView.setOnQueryTextListener(
+                    new SearchView.OnQueryTextListener() {
+
+                        @Override
+                        public boolean onQueryTextSubmit(String query) {
+                            Intent intent = new Intent(MainActivity.this, CurrentActivity.class);
+                            intent.putExtra("TEXT",query);
+                            startActivity(intent);
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onQueryTextChange(String newText) {
+                            return false;
+                        }
+                    }
+            );
+        } else if (id == R.id.loadCity) {
+
+        } else if (id == R.id.saveCity) {
+            openDialog();
+        } else if (id == R.id.search5) {
+
+        } else if (id == R.id.loadCity5) {
+
+        } else if (id == R.id.saveCity5) {
+
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.about) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void openDialog() {
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Do you want to save this city?");
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SAVE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "CANCEL",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+    }
---
+++ ExtendedWeatherTitan/app/src/main/java/com/p92rdi/extendedweathertitan/helper/JsonParser.java
@@ -0,0 +1,48 @@
+package com.p92rdi.extendedweathertitan.helper;
+
+
+import com.p92rdi.extendedweathertitan.model.Weather;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class JsonParser {
+    private static final String TAG_ID = "id";
+    private static final String TAG_SYS = "sys";
+    private static final String TAG_COUNTRY = "country";
+    private static final String TAG_WEATHER = "weather";
+    private static final String TAG_DESCRIPTION = "description";
+    private static final String TAG_ICON = "icon";
+    private static final String TAG_MAIN = "main";
+    private static final String TAG_TEMPERATURE ="temp";
+    private static final String TAG_HUMIDITY = "humidity";
+    private static final String TAG_TEMP_MIN = "temp_min";
+    private static final String TAG_TEMP_MAX= "temp_max";
+    private static final String TAG_WIND = "wind";
+    private static final String TAG_WIND_SPEED = "speed";
+    private static final String TAG_NAME = "name";
+
+    public Weather processWeatherFromJson(String mRawJson) {
+            try {
+                JSONObject jObj = new JSONObject(mRawJson);
+                Weather mProducedWeather = new Weather();
+
+                mProducedWeather.setmCityId(jObj.getInt(TAG_ID));
+                mProducedWeather.setmCountry(jObj.getJSONObject(TAG_SYS).getString(TAG_COUNTRY));
+                mProducedWeather.setmDescription(jObj.getJSONArray(TAG_WEATHER).getJSONObject(0).getString(TAG_DESCRIPTION));
+                mProducedWeather.setmIconCode(jObj.getJSONArray(TAG_WEATHER).getJSONObject(0).getString(TAG_ICON).concat(".png"));
+                mProducedWeather.setmTemperature(jObj.getJSONObject(TAG_MAIN).getInt(TAG_TEMPERATURE));
+                mProducedWeather.setmHumidity(jObj.getJSONObject(TAG_MAIN).getInt(TAG_HUMIDITY));
+                mProducedWeather.setmTempMin(jObj.getJSONObject(TAG_MAIN).getInt(TAG_TEMP_MIN));
+                mProducedWeather.setmTempMax(jObj.getJSONObject(TAG_MAIN).getInt(TAG_TEMP_MAX));
+                mProducedWeather.setmWind(jObj.getJSONObject(TAG_WIND).getInt(TAG_WIND_SPEED));
+                mProducedWeather.setmCity(jObj.getString(TAG_NAME));
+
+                return mProducedWeather;
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+                return new Weather();
+            }
+        }
+    }
